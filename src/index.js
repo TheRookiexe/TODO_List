a171f4b5d5e7bfc63db7679fef0c946429e0b034
TheRookiexe <mishrxa36@gmail.com>
2025-11-09 01:36:28 +0530

    Added Button for Creating New Projects.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import "./style.css";
 const contentDiv = document.getElementById('content');
 const sideBarDiv = document.getElementById('sideBar');
 
+const addProjectBtn = document.createElement('button');
+addProjectBtn.id = "addProjBtn";
+addProjectBtn.textContent = `+   Add New Project`;
+sideBarDiv.appendChild(addProjectBtn);
 
+const barDiv = document.createElement('div');
+barDiv.id = "divisor";
+sideBarDiv.appendChild(barDiv);
